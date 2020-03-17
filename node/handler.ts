ba8d2d227b7d3040feef511c5a8177652bec04fe
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2020-03-17 12:28:51 -0600

    update deps

--- node/handler.ts
+++ node/handler.ts
@@ -1,9 +1,9 @@
-import { APIGatewayProxyHandler } from "aws-lambda";
-import "source-map-support/register";
+import { APIGatewayProxyHandler } from "aws-lambda"
+import "source-map-support/register"
 
 export const node: APIGatewayProxyHandler = async (event, _context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "response body", input: event }, null, 2)
-  };
-};
+  }
+}
